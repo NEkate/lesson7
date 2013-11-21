26d9ab15953d24191f31c5086d8e4e64e58ab317
Kate <katren4@yandex.ru>
2013-11-21 19:44:45 +0200

    точка с модификаторами длины

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 ;(function(){
-    console.log(/./.test('asd'));
+    console.log('asd'.match(/.*/));//asd
+    console.log(''.match(/.*/));//null
+    console.log('asd'.match(/./g));//'a'
+    console.log('asd'.match(/.+/g));//'asd'
+    console.log('asd\nasdasd'.match(/.+/g));//['asd', 'asdasd']
+    console.log(''.match(/.+/g));//null
+    console.log(''.match(/.?/g));//''
+    console.log('asd'.match(/.?/));//a
+
 })();
