0c1c5f837ed11c41196dd13c58b984ca4371f2c3
Kate <katren4@yandex.ru>
2013-11-21 19:56:02 +0200

    конец строки $

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 ;(function(){
-    console.log('\nasd'.match(/^./));//строка должна начинаться со следующего правила // null
-    console.log('\nasd\ndfg\n1sdf'.match(/^\d/m));// \d -строка должна начинаться с числа return null
-    console.log('asd'.match(/^\d/) );// null
-    console.log('1asd'.match(/^\d/) );// 1
-    console.log('asd\n1sdfd'.match(/^\d/) );// null
-    console.log('asd\n1sdfd'.match(/^\d/m) );// 1
+    console.log('asd1asd'.match(/\d$/) );// null
+    console.log('asd1'.match(/\d$/) );// 1
+    console.log('asd1\nsdfd'.match(/\d$/) );// null
+    console.log('asd1\n1sdfd'.match(/\d$/m) );// 1
 
 })();
