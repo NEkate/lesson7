a0451453dc14e6222836b5184a20f5890df9313a
Kate <katren4@yandex.ru>
2013-11-21 20:42:29 +0200

    скобки для группирования данных

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 ;(function(){
-    console.log('asd fdghf qwe'.match(/asd|qwe/));//asd
-    console.log('asd fdghf qwe'.match(/asd|qwe/g));//asd qwe
-    console.log('asd fdghf 1qwe'.match(/\d(asd|qwe)/g));//1qwe
-    console.log('asd fdghf qwe'.match(/asd|qwe|fd/));//asd
-    console.log('asd fdghf qwe'.match(/asd|qwe|fd/g));//asd fd qwe
+    console.log('13px solid #fff'.match(/\d+px/));//13px
+    console.log('13px solid #fff'.match(/(\d+)px/));//13px 13
 })();
